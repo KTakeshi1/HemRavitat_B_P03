f2eca34f83b6e1e00783b87cc619c5aab3e551bd
KTakeshi1 <takeshik623@gmail.com>
2023-12-05 01:41:19 -0600

    1st Stable Dialogue Build
    Very bare bones, but it currently gets the job done. Will add more basic functionality.

--- Assets/Scripts/DialogueTool.cs
+++ Assets/Scripts/DialogueTool.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 public class DialogueTool : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text dialogueText;
+    public TMP_Text nameText;
+    public Image characterImage;
+    private int currentLine = 0;
+    public Dialogue[] dialogues;
+    //public Dialogue[] names;
+
     void Start()
     {
-        
+        ShowDialogue();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ContinueDialogue();
+        }
+    }
+
+    void ShowDialogue()
+    {
+        dialogueText.text = dialogues[currentLine].dialogue;
+        nameText.text = dialogues[currentLine].characterName;
+        characterImage.sprite = dialogues[currentLine].speakerImage;
+    }
+
+    public void ContinueDialogue()
+    {
+        currentLine++;
+
+        if (currentLine < dialogues.Length)
+        {
+            ShowDialogue();
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    void EndDialogue()
     {
         
     }
+}
+
+[System.Serializable]
+public class Dialogue
+{
+    public string characterName;
+    public string dialogue;
+    public Sprite speakerImage;
 }
