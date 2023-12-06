ff4fe0d88dd9fa7ffcc855792c21fb529fc341ea
KTakeshi1 <takeshik623@gmail.com>
2023-12-05 20:24:40 -0600

    Wrapped up tool coding
    Final stable build

--- Assets/Scripts/DialogueTool.cs
+++ Assets/Scripts/DialogueTool.cs
@@ -6,11 +6,26 @@ using UnityEngine;
 
 public class DialogueTool : MonoBehaviour
 {
-    public TMP_Text dialogueText;
-    public TMP_Text nameText;
-    public Image characterImage;
+    [SerializeField]
+    private GameObject dialogueMaster;
+    [Header("Basic Setup")]
+    [SerializeField][Tooltip("Drag dialogue text object from scene/hiearchy to here")]
+    private TMP_Text dialogueTextObject;
+    [SerializeField][Tooltip("Drag character name text object from scene/hiearchy to here")]
+    private TMP_Text nameTextObject;
+    [SerializeField][Tooltip("Drag character image object from scene/hiearchy to here (make sure it's blank)")]
+    private Image characterImageObject;
+
+    [Header("Text Colors Customization")]
+    [SerializeField]
+    private Color dialogueColor = Color.black;
+    [SerializeField]
+    private Color nameColor = Color.white;
     private int currentLine = 0;
-    public Dialogue[] dialogues;
+    
+    [Header("Dialogue Script")]
+    [SerializeField]
+    private Dialogue[] dialogues;
     //public Dialogue[] names;
 
     void Start()
@@ -28,9 +43,11 @@ public class DialogueTool : MonoBehaviour
 
     void ShowDialogue()
     {
-        dialogueText.text = dialogues[currentLine].dialogue;
-        nameText.text = dialogues[currentLine].characterName;
-        characterImage.sprite = dialogues[currentLine].speakerImage;
+        dialogueTextObject.color = dialogueColor;
+        nameTextObject.color = nameColor;
+        dialogueTextObject.text = dialogues[currentLine].dialogue;
+        nameTextObject.text = dialogues[currentLine].characterName;
+        characterImageObject.sprite = dialogues[currentLine].characterImage;
     }
 
     public void ContinueDialogue()
@@ -49,14 +66,22 @@ public class DialogueTool : MonoBehaviour
 
     void EndDialogue()
     {
-        
+        /*Users may handle this part in any way of their choosing, but just for all intents and purpose I will
+        hide the UI once the dialogue has ended*/
+        dialogueMaster.SetActive(false);
     }
 }
 
 [System.Serializable]
 public class Dialogue
 {
+    [Header("Lines")]
+    [SerializeField][Tooltip("This is where you add the name of the character")]
     public string characterName;
+    [SerializeField][Tooltip("This is where you add the image for the character")]
+    public Sprite characterImage;
+    [SerializeField][Tooltip("This is where you add dialogue for the textbox")]
+    [TextArea()]
     public string dialogue;
-    public Sprite speakerImage;
+    
 }
